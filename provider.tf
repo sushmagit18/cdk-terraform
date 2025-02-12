ab4d997aaa02aa08f7e475dbd2f47f0b963b5d96
sushmagit18 <gmsusshma2023@gmail.com>
2025-02-11 21:57:13 -0500

    Added

--- provider.tf
+++ provider.tf
@@ -7,6 +7,13 @@ terraform {
   }
 
   required_version = ">= 1.2.0"
+  backend "s3" {
+    bucket         = "myawsbucket-jan-22"  # Replace with your S3 bucket name
+    key            = "terraform.tfstate"          # State file location within the bucket
+    region         = "var.region"                  # AWS region
+  }
+
+
 }
 
 provider "aws" {
